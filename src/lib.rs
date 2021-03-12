9c758a9f9434651b3b88bbdcd0ce5198345d2eab
FISH UP <MisterFISHUP@users.noreply.github.com>
2021-03-12 22:39:22 +0100

    use channel

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,26 @@
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Mutex;
+use std::sync::Arc;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 struct Worker {
     id: usize,
     thread: thread::JoinHandle<()>,
-    
 }
 
 impl Worker {
-    fn new(id: usize ) -> Worker {
-        let thread = thread::spawn(||{});
-        Worker {id, thread}
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+        Worker { id, thread }
     }
 }
 impl ThreadPool {
@@ -26,12 +33,17 @@ impl ThreadPool {
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
-        for i in 0..size {
-            workers.push(Worker::new(i));
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
